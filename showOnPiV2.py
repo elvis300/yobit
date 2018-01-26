e1f6a787a8cf5ff2396cad8c73149b01404ecaf3
elvis300 <35616354+elvis300@users.noreply.github.com>
2018-01-27 00:51:01 +0200

    Update showOnPiV2.py

--- showOnPiV2.py
+++ showOnPiV2.py
@@ -4,7 +4,16 @@ from oled.device import ssd1306, sh1106
 from oled.render import canvas
 from PIL import ImageFont
 
-
+padding = 2
+spacing = 30
+x= padding
+shape_width = 20
+top = padding
+raw = top
+bottom = device.height - padding - 1 
+font = ImageFont.load_default()
+device = sh1106(port=1, address=0x3C)
+yo = YoBit()
 
 def formated_ticker2(yo, pair):
     print "formated_ticker with ",pair
@@ -13,50 +22,27 @@ def formated_ticker2(yo, pair):
         if key == "last":
             return val
 
+def printer(yo, pair,x, raw ):
+
+	with canvas(device) as draw:
+		lastPrice = formated_ticker2(yo,pair)  
+    	draw.text((x, raw),    'BTC',  font=font, fill=255)
+    	draw.text((x+spacing , raw), str(lastPrice), font=font, fill=255)
+		raw = raw + 10
 
 def main():
-    
-  font = ImageFont.load_default()
-  device = sh1106(port=1, address=0x3C)
-  yo = YoBit() 
-  with canvas(device) as draw:
-    # Draw some shapes.
-    # First define some constants to allow easy resizing of shapes.
-    padding = 2
-    spacing = 30
-    x= padding
-    shape_width = 20
-    top = padding
-    raw = top
-    bottom = device.height - padding - 1
-    
-    
-    pair = "btc_usd"
-    last = formated_ticker2(yo,pair)  
+ 
 
-    draw.text((x, raw),    'BTC',  font=font, fill=255)
-    draw.text((x+spacing , raw), str(last), font=font, fill=255)
+  printer(yo,"btc_usd")
+  printer(yo,"dcr_btc")
+  printer(yo,"etc_btc")
+  printer(yo,"cme_btc")
     
-    pair = "dcr_btc"
-    last = formated_ticker2(yo,pair)
     x = padding
     raw = raw + 10
     draw.text((x, raw),    'DCR',  font=font, fill=255)
     draw.text((x+spacing , raw), str(last), font=font, fill=255)
     
-    pair = "etc_btc"
-    last = formated_ticker2(yo,pair)  
-    x = padding
-    raw = raw + 10
-    draw.text((x, raw),    'ETC',  font=font, fill=255)
-    draw.text((x+spacing , raw), str(last), font=font, fill=255)
-    
-    pair = "cme_btc"
-    last = formated_ticker2(yo,pair)
-    x = padding
-    raw = raw + 10
-    draw.text((x, raw),    'CME',  font=font, fill=255)
-    draw.text((x+spacing , raw), str(last), font=font, fill=255)
 
 if __name__ == '__main__':
     main()
