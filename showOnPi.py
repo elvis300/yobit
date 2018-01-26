43da0e36f3e7dba58d933312e272e51ef2504601
root <elvis3002@walla.com>
2018-01-26 21:52:23 +0000

    fix showOnPi

--- showOnPi.py
+++ showOnPi.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
-
-# Ported from:
-# https://github.com/adafruit/Adafruit_Python_SSD1306/blob/master/examples/shapes.py
-
+from YoBit import YoBit
 from oled.device import ssd1306, sh1106
 from oled.render import canvas
 from PIL import ImageFont
-import imp
 
-font = ImageFont.load_default()
-device = sh1106(port=1, address=0x3C)
 
-with canvas(device) as draw:
+
+def formated_ticker2(yo, pair):
+    print "in formated_ticker"
+    ticker = yo.ticker(pair)
+    for key, val in ticker[pair].iteritems():
+        if key == "last":
+            return val
+
+
+def main():
+    
+  font = ImageFont.load_default()
+  device = sh1106(port=1, address=0x3C)
+
+  with canvas(device) as draw:
     # Draw some shapes.
     # First define some constants to allow easy resizing of shapes.
     padding = 2
@@ -19,15 +27,17 @@ with canvas(device) as draw:
     shape_width = 20
     top = padding
     bottom = device.height - padding - 1
- 
-
-    # Load default font.
     font = ImageFont.load_default()
 
-    # Alternatively load a TTF font.
-    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-    # font = ImageFont.truetype('Minecraftia.ttf', 8)
+    pair = "dcr_btc"
+    yo = YoBit() 
+    last = formated_ticker2(yo,pair)
+    print last
+    print str(last)
 
-    # Write two lines of text.
     draw.text((x, top),    'decred',  font=font, fill=255)
-    draw.text((x+40 , top), '123!', font=font, fill=255)
+    draw.text((x+40 , top), str(last), font=font, fill=255)
+
+
+if __name__ == '__main__':
+    main()
